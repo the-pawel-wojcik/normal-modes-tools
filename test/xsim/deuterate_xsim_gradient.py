ecc54c0dee5ba3eb409e15b89f9c9e1949e3668e
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-03-04 17:57:23 -0800

    Print grad to xsim_json; add argparse for input

--- test/xsim/deuterate_xsim_gradient.py
+++ test/xsim/deuterate_xsim_gradient.py
@@ -1,10 +1,27 @@
 import normal_modes_tools as nmt
 import numpy as np
 import json
+import argparse
+
 
 gradient_json_filename = 'inputs/sroph_at_g0_kappa_a.json'
 nmodes_xyz_fname = 'inputs/sroph_f0.xyz'
 deuterated_nmodes_xyz_fname = 'outputs/sroph-5d_f0.xyz'
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--gradient_fname',
+        default=gradient_json_filename,
+    )
+    parser.add_argument(
+        '--debug',
+        type=bool,
+        default=False,
+        action='store_true',
+    )
+    args = parser.parse_args()
+    return args
 
 
 def print_np_vec_with_atoms(
@@ -30,15 +47,22 @@ def print_gradient_in_normal_modes(
               f': {grad_comp:4.0f}')
 
 
-def gradient_from_json(
-    gradient_json_filename: str
-) -> np.typing.NDArray:
+def collect_gradient_from_json(
+    gradient_json_filename: str,
+) -> dict:
     with open(gradient_json_filename, 'r', encoding='utf-8') as grad_json_file:
         grad_input = json.load(grad_json_file)
+    return  grad_input
 
-    dim = 33
+
+def gradient_json_to_numpy(
+    gradient_json: dict,
+) -> np.typing.NDArray:
+
+    dim = len(gradient_json['gradient'])
+
     grad_input_np = np.zeros(shape=(dim), dtype=float)
-    for idx, component in enumerate(grad_input['gradient']):
+    for idx, component in enumerate(gradient_json['gradient']):
         grad_value = component['gradient, cm-1']
         grad_input_np[idx] = grad_value
 
@@ -46,19 +70,24 @@ def gradient_from_json(
 
 
 def main():
+    args = get_args()
+    DEBUG = args.debug
     nmodes = nmt.xyz_file_to_NormalModesList(nmodes_xyz_fname)
     ref_geo = nmodes[0].at
 
-    grad_input_np = gradient_from_json(gradient_json_filename)
-    print_gradient_in_normal_modes(grad_input_np, nmodes)
+    gradient_json = collect_gradient_from_json(args.gradient_fname)
+    grad_input_np = gradient_json_to_numpy(gradient_json)
+    if DEBUG:
+        print_gradient_in_normal_modes(grad_input_np, nmodes)
 
     mass_matrix = nmt.build_mass_matrix(ref_geo, nmt.ATOMIC_MASSES)
     nmodes_matrix = nmt.build_nmodes_matrix(nmodes)
     mass_matrix_sqrt = np.sqrt(mass_matrix)
     grad_descartes = mass_matrix_sqrt @ nmodes_matrix @ grad_input_np
 
-    print("Gradient in Cartesian coordinates")
-    print_np_vec_with_atoms(grad_descartes, ref_geo.atoms)
+    if DEBUG:
+        print("Gradient in Cartesian coordinates")
+        print_np_vec_with_atoms(grad_descartes, ref_geo.atoms)
 
     deuterated_nmodes = nmt.xyz_file_to_NormalModesList(
         deuterated_nmodes_xyz_fname
@@ -66,10 +95,27 @@ def main():
     deuterated_nmodes_matrix = nmt.build_nmodes_matrix(deuterated_nmodes)
     deuterated_mm = nmt.build_mass_matrix(ref_geo, nmt.DEUTERATED_MASSES)
     deuterated_mm_inv_sqrt = nmt.get_mass_inv_sqrt(deuterated_mm)
-    grad_deuterated_nmodes = \
+    deuterated_gradient = \
         deuterated_nmodes_matrix.T @ deuterated_mm_inv_sqrt @ grad_descartes
 
-    print_gradient_in_normal_modes(grad_deuterated_nmodes, deuterated_nmodes)
+    if DEBUG:
+        print_gradient_in_normal_modes(
+            gradient=deuterated_gradient,
+            nmodes=deuterated_nmodes,
+        )
+
+    outpack = {
+        'gradient': list(),
+        'EOM states': gradient_json['EOM states'],
+    }
+    for idx, (mode, grad_comp) in enumerate(zip(nmodes, deuterated_gradient)):
+        outpack['gradient'].append({
+            'mode #': idx,
+            'frequency, cm-1': mode.frequency,
+            'gradient, cm-1': grad_comp,
+        })
+
+    print(json.dumps(outpack))
 
     
 if __name__ == "__main__":

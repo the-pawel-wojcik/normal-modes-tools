aca433e36509429eb4e7e2494603c9b300ba9903
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-03-22 12:49:51 -0400

    Add pretty print by Mulliken label

---
+++ src/normal_modes_tools/mulliken/core.py
@@ -0,0 +1,40 @@
+from normal_modes_tools import NormalMode
+
+irrep_to_order = {
+    "C2v": {
+        "A1": 0,
+        "A2": 1,
+        "B1": 2,
+        "B2": 3,
+    }, # From Herzberg's Book Table 13
+}
+
+
+def sort_Mulliken(
+    mode: NormalMode,
+    point_group: str = "C2v",
+    irrep: str = "A1",
+) -> tuple[int, float]:
+    """ A key for sorting a list of `NormalMode` objects by the order suggested
+    in Mulliken's convention [1]. Sort the modes first by the irrep, and then
+    by frequency. Highest frequency goes first.
+
+    [1] R. S. Mulliken, Report on Notation for the Spectra of Polyatomic
+    Molecules, The Journal of Chemical Physics 23, 1997 (1955).
+    """
+    if mode.at.point_group != "":
+        point_group = mode.at.point_group
+    
+    if mode.irrep != "":
+        irrep = mode.irrep
+
+    irrep_position = irrep_to_order[point_group][irrep]
+
+    return irrep_position, -mode.frequency
+
+
+def pretty_print(nmodes: list[NormalMode]):
+    nmodes.sort(key=sort_Mulliken)
+    for idx, mode in enumerate(nmodes, start=1):
+        print(f'{idx:3d}({mode.irrep.lower()}): {mode.frequency:>4.0f}')
+    
